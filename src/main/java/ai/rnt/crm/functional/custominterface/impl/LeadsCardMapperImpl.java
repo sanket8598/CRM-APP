8b87fb95ccdccdad6eae698722641415ad0aa6bc
Sanket Wakankar <Sanket Wakankar@RNT-PUN-1156.ho.rnt.ai>
2023-11-21 11:26:52 +0530

    chnages done in the card display edit added the constant and the validations.

--- src/main/java/ai/rnt/crm/functional/custominterface/impl/LeadsCardMapperImpl.java
+++ src/main/java/ai/rnt/crm/functional/custominterface/impl/LeadsCardMapperImpl.java
@@ -1,5 +1,15 @@
 package ai.rnt.crm.functional.custominterface.impl;
 
+import static ai.rnt.crm.constants.LeadEntityFieldConstant.BUDGET_AMOUNT;
+import static ai.rnt.crm.constants.LeadEntityFieldConstant.COMPANY_NAME;
+import static ai.rnt.crm.constants.LeadEntityFieldConstant.DESIGNATION;
+import static ai.rnt.crm.constants.LeadEntityFieldConstant.LEAD_ASSIGN_USERNAME;
+import static ai.rnt.crm.constants.LeadEntityFieldConstant.LEAD_NAME;
+import static ai.rnt.crm.constants.LeadEntityFieldConstant.LEAD_SOURCE;
+import static ai.rnt.crm.constants.LeadEntityFieldConstant.SERVICE_FALLS_INTO;
+import static ai.rnt.crm.constants.LeadEntityFieldConstant.TOPIC;
+import static java.util.Objects.nonNull;
+
 import ai.rnt.crm.dto.LeadsCardDto;
 import ai.rnt.crm.entity.Leads;
 import ai.rnt.crm.functional.custominterface.LeadsCardMapper;
@@ -16,24 +26,31 @@ public class LeadsCardMapperImpl implements LeadsCardMapper {
 		leadsCardDto.setDisqualifyAs(lead.getDisqualifyAs());
 		leadsCardDto.setStatus(lead.getStatus());
 		leadsCardDto.setImportant(lead.getImportant());
+
 		PrimaryFieldMapper primaryFieldMapper = getPrimaryFieldMapper(primaryField);
 		SecondaryFieldMapper secondaryFieldMapper = getSecondaryFieldMapper(secondaryField);
 
 		primaryFieldMapper.apply(lead, leadsCardDto);
 		secondaryFieldMapper.apply(lead, leadsCardDto);
-
 		return leadsCardDto;
 	}
 
 	private PrimaryFieldMapper getPrimaryFieldMapper(String primaryField) {
 		switch (primaryField) {
-		case "Lead Name":
+		case LEAD_NAME:
 			return (lead, leadsCardDto) -> {
 				leadsCardDto.setPrimaryField(lead.getFirstName() + " " + lead.getLastName());
 				leadsCardDto.setShortName(LeadsCardUtil.shortName(lead.getFirstName(), lead.getLastName()));
 			};
-		case "Company Name":
-			return (lead, leadsCardDto) -> leadsCardDto.setPrimaryField(lead.getCompanyMaster().getCompanyName());
+		case COMPANY_NAME:
+			return (lead, leadsCardDto) -> {
+				leadsCardDto.setPrimaryField(
+						nonNull(lead.getCompanyMaster()) ? lead.getCompanyMaster().getCompanyName() : null);
+				leadsCardDto.setShortName(
+						nonNull(lead.getCompanyMaster()) && nonNull(lead.getCompanyMaster().getCompanyName())
+								? LeadsCardUtil.shortName(lead.getCompanyMaster().getCompanyName())
+								: null);
+			};
 		default:
 			return (lead, leadsCardDto) -> {
 				leadsCardDto.setPrimaryField(lead.getFirstName() + " " + lead.getLastName());
@@ -44,25 +61,29 @@ public class LeadsCardMapperImpl implements LeadsCardMapper {
 
 	private SecondaryFieldMapper getSecondaryFieldMapper(String secondaryField) {
 		switch (secondaryField) {
-		case "Lead Name":
+		case LEAD_NAME:
 			return (lead, leadsCardDto) -> leadsCardDto
 					.setSecondaryField(lead.getFirstName() + " " + lead.getLastName());
-		case "Topic":
+		case TOPIC:
 			return (lead, leadsCardDto) -> leadsCardDto.setSecondaryField(lead.getTopic());
-		case "Company Name":
-			return (lead, leadsCardDto) -> leadsCardDto.setSecondaryField(lead.getCompanyMaster().getCompanyName());
-		case "Designation":
+		case COMPANY_NAME:
+			return (lead, leadsCardDto) -> leadsCardDto.setSecondaryField(
+					nonNull(lead.getCompanyMaster()) ? lead.getCompanyMaster().getCompanyName() : null);
+		case DESIGNATION:
 			return (lead, leadsCardDto) -> leadsCardDto.setSecondaryField(lead.getDesignation());
-		case "Budget Amount":
+		case BUDGET_AMOUNT:
 			return (lead, leadsCardDto) -> leadsCardDto.setSecondaryField(lead.getBudgetAmount());
-		case "Service Falls Into":
-			return (lead, leadsCardDto) -> leadsCardDto
-					.setSecondaryField(lead.getServiceFallsMaster().getServiceName());
-		case "Lead Source":
-			return (lead, leadsCardDto) -> leadsCardDto.setSecondaryField(lead.getLeadSourceMaster().getSourceName());
-		case "Lead Assign Username":
-			return (lead, leadsCardDto) -> leadsCardDto
-					.setSecondaryField(lead.getEmployee().getFirstName() + " " + lead.getEmployee().getLastName());
+		case SERVICE_FALLS_INTO:
+			return (lead, leadsCardDto) -> leadsCardDto.setSecondaryField(
+					nonNull(lead.getServiceFallsMaster()) ? lead.getServiceFallsMaster().getServiceName() : null);
+		case LEAD_SOURCE:
+			return (lead, leadsCardDto) -> leadsCardDto.setSecondaryField(
+					nonNull(lead.getLeadSourceMaster()) ? lead.getLeadSourceMaster().getSourceName() : null);
+		case LEAD_ASSIGN_USERNAME:
+			return (lead,
+					leadsCardDto) -> leadsCardDto.setSecondaryField(nonNull(lead.getEmployee())
+							? lead.getEmployee().getFirstName() + " " + lead.getEmployee().getLastName()
+							: null);
 		default:
 			return (lead, leadsCardDto) -> leadsCardDto.setSecondaryField(lead.getTopic());
 		}
