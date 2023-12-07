c1b4001224a4d625855a07f146b9e838a68c92c5
sanket <sanketwakankar8@gmail.com>
2023-12-08 00:35:54 +0530

    done

--- src/main/java/ai/rnt/crm/util/ConvertDateFormatUtil.java
+++ src/main/java/ai/rnt/crm/util/ConvertDateFormatUtil.java
@@ -1,17 +1,22 @@
 package ai.rnt.crm.util;
 
+import static ai.rnt.crm.constants.DateFormatterConstant.DATE_TIME_WITH_AM_AND_PM;
+import static ai.rnt.crm.constants.DateFormatterConstant.DD_MMM_YYYY;
+import static ai.rnt.crm.constants.DateFormatterConstant.DEFAULT_UTIL_DATE_FORMAT;
+import static ai.rnt.crm.constants.DateFormatterConstant.YYYY_MM_DD;
+import static java.time.ZoneId.systemDefault;
+import static java.util.Date.from;
+
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.Date;
 
 import ai.rnt.crm.exception.CRMException;
 import lombok.extern.slf4j.Slf4j;
 
-
 /**
- * This class is used to get the required formattedDates through methods and functions.
+ * This class is used to get the required formattedDates through methods and
+ * functions.
  * 
  * @author Sanket Wakankar
  * @version 1.0
@@ -26,7 +31,7 @@ public class ConvertDateFormatUtil {
 	}
 
 	/**
-	 * @author Nikhil Gaikwad
+	 * @author Sanket Wakankar
 	 * @version 1.0
 	 * @since 06/09/2023.
 	 * @param toConvertDate
@@ -35,8 +40,8 @@ public class ConvertDateFormatUtil {
 
 	public static String convertDate(LocalDateTime toConvertDate) {
 		try {
-			return new SimpleDateFormat("dd-MMM-yyyy hh:mm a").format(new SimpleDateFormat("E MMM dd HH:mm:ss z yyyy")
-					.parse(Date.from(toConvertDate.atZone(ZoneId.systemDefault()).toInstant()).toString()));
+			return DATE_TIME_WITH_AM_AND_PM.format(DEFAULT_UTIL_DATE_FORMAT
+					.parse(from(toConvertDate.atZone(systemDefault()).toInstant()).toString()));
 		} catch (Exception e) {
 			log.error("Got Excetion while converting date format:", e.getMessage());
 			throw new CRMException("error while date conversion");
@@ -45,31 +50,31 @@ public class ConvertDateFormatUtil {
 
 	public static String convertDateUtilDate(Date date) throws ParseException {
 		try {
-			return new SimpleDateFormat("dd-MMM-yyyy")
-					.format(new SimpleDateFormat("E MMM dd HH:mm:ss z yyyy").parse(date.toString()));
+			return DD_MMM_YYYY.format(DEFAULT_UTIL_DATE_FORMAT.parse(date.toString()));
 		} catch (Exception e) {
 			log.error("Got Excetion while converting date format in convertDateUtilDate:", e.getMessage());
 			throw new CRMException("error while date conversion");
 		}
 	}
-	
+
 	/**
-	 * This method is used to convert the given date(yyyy-MM-dd) to the format (dd-MMM-yyyy) with give String time added to it.
-	 *  e.g 2023-12-04 is converted to the  04 Dec 2023 and also time is added.
+	 * This method is used to convert the given date(yyyy-MM-dd) to the format
+	 * (dd-MMM-yyyy) with give String time added to it. e.g 2023-12-04 is converted
+	 * to the 04 Dec 2023 and also time is added.
+	 * 
 	 * @author Sanket Wakankar
 	 * @version 1.0
 	 * @since 05/12/2023.
 	 * @param date in java.util.Date,Time in String
 	 * @return String
 	 */
-	public static String convertDateDateWithTime(Date date,String endTime) {
+	public static String convertDateDateWithTime(Date date, String endTime) {
 		try {
-			return new SimpleDateFormat("dd-MMM-yyyy")
-					.format(new SimpleDateFormat("yyyy-MM-dd").parse(date.toString()))+" "+endTime;
+			return DD_MMM_YYYY.format(YYYY_MM_DD.parse(date.toString()))+ " " +endTime;
 		} catch (Exception e) {
 			log.error("Got Excetion while converting date format in convertDateDateWithTime:", e.getMessage());
 			throw new CRMException("error while date conversion");
 		}
 	}
-	
+
 }
