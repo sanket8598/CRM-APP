8d588c0b8f7d03a9e730382bf32ddcc27b80472b
Sanket Wakankar <Sanket Wakankar@RNT-PUN-1156.ho.rnt.ai>
2023-12-14 12:18:55 +0530

    Changes done in contact.

--- src/main/java/ai/rnt/crm/service/ContactService.java
+++ src/main/java/ai/rnt/crm/service/ContactService.java
@@ -2,8 +2,6 @@ package ai.rnt.crm.service;
 
 import java.util.EnumMap;
 
-import javax.validation.Valid;
-
 import org.springframework.http.ResponseEntity;
 
 import ai.rnt.crm.dto.ContactDto;
@@ -11,7 +9,7 @@ import ai.rnt.crm.enums.ApiResponse;
 
 public interface ContactService {
 
-	ResponseEntity<EnumMap<ApiResponse, Object>> addContact(@Valid ContactDto contactDto, Integer leadId);
+	ResponseEntity<EnumMap<ApiResponse, Object>> addContact(ContactDto contactDto, Integer leadId);
 
 	ResponseEntity<EnumMap<ApiResponse, Object>> updateContact(ContactDto contactDto, Integer contactId);
 
