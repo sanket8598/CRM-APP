cdee345488aa0136e829ff701bc61a54bb68fd28
Nikhil Gaikwad <Nikhil Gaikwad@RNT-PUN-1030.ho.rnt.ai>
2024-01-22 18:52:58 +0530

    Created regex constants.

--- src/main/java/ai/rnt/crm/constants/RegexConstant.java
+++ src/main/java/ai/rnt/crm/constants/RegexConstant.java
@@ -1,7 +1,27 @@
 package ai.rnt.crm.constants;
 
-public final class RegexConstant {
-	
-	public static final String IS_DIGIT="^\\d+$";
+import static lombok.AccessLevel.PRIVATE;
 
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Sanket Wakankar
+ * @since 22/01/2024
+ * @version 1.0
+ *
+ */
+@NoArgsConstructor(access = PRIVATE)
+public final class RegexConstant {
+
+	public static final String IS_DIGIT = "^\\d+$";
+
+	public static final String IS_EMAIL = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
+
+	public static final String IS_FNAME_LNAME = "^[a-zA-Z]+$";
+
+	public static final String IS_STRING = "^[a-zA-Z\\s]+$";
+
+	public static final String IS_PHONE_NUMBER = "^\\+?\\d{10,13}$";
+
+	public static final String IS_AMOUNT = "^\\d+(?:\\.\\d+)?$";
 }
