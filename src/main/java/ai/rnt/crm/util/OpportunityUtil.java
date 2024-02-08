7f0d95cf57a8cd789233f84c1bc64667c565b162
Nikhil Gaikwad <Nikhil Gaikwad@RNT-PUN-1030.ho.rnt.ai>
2024-02-08 19:20:38 +0530

    Changes done.

--- src/main/java/ai/rnt/crm/util/OpportunityUtil.java
+++ src/main/java/ai/rnt/crm/util/OpportunityUtil.java
@@ -4,37 +4,44 @@ import static ai.rnt.crm.constants.OppurtunityStatus.ANALYSIS;
 import static ai.rnt.crm.constants.OppurtunityStatus.CLOSE;
 import static ai.rnt.crm.constants.OppurtunityStatus.PROPOSE;
 import static ai.rnt.crm.constants.OppurtunityStatus.QUALIFY;
+import static java.lang.String.format;
 import static java.lang.String.valueOf;
 import static lombok.AccessLevel.PRIVATE;
 
 import lombok.NoArgsConstructor;
 
+/**
+ * @author Nikhil Gaikwad
+ * @since 08/02/2024
+ * @version 2.0
+ *
+ */
 @NoArgsConstructor(access = PRIVATE)
 public class OpportunityUtil {
 
-	private static String[] c = new String[] { "K", "L", "Cr" };
+	private static String[] suffix = new String[] { "K", "L", "Cr" };
 
 	public static String amountInWords(double n) {
 		long wholePart = (long) n;
 		double fractionalPart = n - wholePart;
 		int size = valueOf(wholePart).length();
 		if (size <= 3) {
-			return format((wholePart + fractionalPart) / 1000.0, c[0]);
+			return formatAmount((wholePart + fractionalPart) / 1000.0, suffix[0]);
 		} else if (size >= 4 && size < 6) {
-			return format((wholePart + fractionalPart) / 1000.0, c[0]);
+			return formatAmount((wholePart + fractionalPart) / 1000.0, suffix[0]);
 		} else if (size >= 6 && size < 8) {
-			return format((wholePart + fractionalPart) / 100000.0, c[1]);
+			return formatAmount((wholePart + fractionalPart) / 100000.0, suffix[1]);
 		} else if (size >= 8) {
-			return format((wholePart + fractionalPart) / 10000000.0, c[2]);
+			return formatAmount((wholePart + fractionalPart) / 10000000.0, suffix[2]);
 		} else {
 			return valueOf(n);
 		}
 	}
 
-	private static String format(double value, String suffix) {
-		return String.format("%.1f%s", value, suffix);
+	private static String formatAmount(double value, String suffix) {
+		return format("%.1f%s", value, suffix);
 	}
-	
+
 	public static Double calculateBubbleSize(double opportunityBudget, double totalBudget) {
 		return (opportunityBudget / totalBudget) * 100.0;
 	}
